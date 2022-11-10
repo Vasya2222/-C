83fc147dd3d82bc269f595e0e0a43e6ee0eba2f1
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-10 22:54:16 +0300

    Добавили комментарии, что делает программа

--- Lesson0002/Program.cs
+++ Lesson0002/Program.cs
@@ -1,4 +1,6 @@
-﻿string username = Console.ReadLine();
-
+﻿// Переменная веденная с клавиатуры пользователя
+string username = Console.ReadLine();
+// Выводим приветсвие
 Console.Write("Привет, ");
+// Выводим переменную
 Console.WriteLine(username + '!');
