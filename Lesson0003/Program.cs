0f015aac50745a225821505e9ea6fe3326e4bb6d
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-10 23:07:30 +0300

    Сумма двух чисел

--- Lesson0003/Program.cs
+++ Lesson0003/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Вывести сумму двух чисел
+
+// Объявляем две переменные при помощи целочисленного типа данных int
+
+int number = 10;
+int number1 = 10;
+
+Console.Write("Сумма двух числе - ");
+Console.WriteLine(number + number1);
