d47e87c542dfa047d1c53dd8fcbb86ba03240eba
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-11 19:43:36 +0300

    какая-то ошибка

--- Lesson0006/Program.cs
+++ Lesson0006/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Поприветсвовать пользователя запросив имя и усовершенствовать арифметические операторы(калькулятор)
+
+// запросить имя пользователя
+Console.Write("Введите Ваше имя -> ");
+string name = Console.ReadLine();
+// поприветствовать пользователя
+
+Console.Write("Привет, ");
+Console.WriteLine(name);
+
+// Ввод чисел
+
+// Запрос пользователю ввести числа
+
+Console.Write("Введите 1 число -> ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число -> ");
+int num1 = int.Parse(Console.ReadLine());
+
+
+// запрос пользователю ввести оператор +, -, *, /, %
+
+Console.Write("Введите оператор +, -, *, /, %");
+string op = Console.ReadLine();
+
+// Проверка условий
+
+if (op == '+'){
+    Console.Write("Сумма чисел равна -> ");
+    Console.WriteLine(num + num1);
+}
+
+else if(op == '-'){
+    Console.Write("Вычитание чисел равна -> ");
+    Console.WriteLine(num - num1);
+}
+
+else if(op == '*'){
+    Console.Write("Произведение чисел равна -> ");
+    Console.WriteLine(num * num1);
+}
+
+else if(op == '/'){
+    Console.Write("Деление чисел равна -> ");
+    Console.WriteLine(num / num1);
+}
+
+else if(op == '%'){
+    Console.Write("Деление по модулю чисел равна -> ");
+    Console.WriteLine(num % num1);
+}
+
+else{
+    Console.WriteLine("Вы ввели не тот оператор");
+}
