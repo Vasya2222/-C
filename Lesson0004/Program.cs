288dc96c391288f8efc84dd20b384c4df180c081
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-11 13:42:13 +0300

    Исправил некоторые недочеты

--- Lesson0004/Program.cs
+++ Lesson0004/Program.cs
@@ -3,27 +3,27 @@
 // вычитание, деление с остатком введенным пользователем
 
 // Приветствуем пользователя
-Console.WriteLine("Введите ваше имя ->");
+Console.Write("Введите ваше имя -> ");
 string name = Console.ReadLine();
 Console.Write("Привет, ");
 Console.WriteLine(name + "!");
 
 // Вести  числа
-Console.WriteLine("Введите числа ->");
+Console.WriteLine("Введите числа");
 
-Console.Write("Введите первое число ->");
+Console.Write("Введите первое число -> ");
 int number = int.Parse(Console.ReadLine());
 
-Console.Write("Введите второе число ->");
+Console.Write("Введите второе число -> ");
 int number1 = int.Parse(Console.ReadLine());
 
 
 // Вывести сумму чисел
 
-Console.Write("Сумма чисел равна ->");
+Console.Write("Сумма чисел равна -> ");
 Console.WriteLine(number + number1);
 
 // Ввывести вычитание чисел
 
-Console.Write("Вычитание чисел равна ->");
+Console.Write("Вычитание чисел равна -> ");
 Console.WriteLine(number - number1);
