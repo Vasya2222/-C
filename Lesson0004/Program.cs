3149a950767ee8fb3b1a18638e205c4e1d7c7d21
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-11 13:34:33 +0300

    Добавили приветствие пользователя и вести числа

--- Lesson0004/Program.cs
+++ Lesson0004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача: Поприветсвовать пользователя и вывести сумму, произведение, деление,
+// вычитание, деление с остатком введенным пользователем
+
+// Приветствуем пользователя
+Console.WriteLine("Введите ваше имя ->");
+string name = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(name + "!");
+
+// Вести  числа
+Console.WriteLine("Введите числа ->");
+
+Console.Write("Введите первое число ->");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ->");
+int number1 = int.Parse(Console.ReadLine());
