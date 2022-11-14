5b8fc69ebba046cc0a08cc1c659ff666009401b2
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-14 21:30:27 +0300

    ищем число введенным пользователем в массиве

---
+++ Lesson0011/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача: имеется одномерный массив array из n элементов. требуется 
+// найти элемент массива, равный find
+
+Console.WriteLine("Код для проверки числа в массиве");
+
+Console.Write("Введите число для нахождения числа в массиве -> ");
+int find = Convert.ToInt32(Console.ReadLine());
+
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+int index = 0;
+
+while (index < 9){
+    if (array[index] == find){
+        Console.WriteLine("Да число есть!");
+        break;
+    }
+    index++;
+}
