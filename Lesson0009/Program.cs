6ec72270fa283cbebaccd01a84a959238171e16e
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-14 20:00:49 +0300

    нашли маскимальное число первым способом

--- Lesson0009/Program.cs
+++ Lesson0009/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Console.WriteLine("Первый способ");
+int num1 = 12;
+int num2 = 1;
+int num3 = 20;
+int num4 = 100;
+int num5 = 54;
+int num6 = 60;
+int num7 = 80;
+int num8 = 8888;
+int num9 = 8822;
+int num10 = 2;
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+if (num4 > max) max = num4;
+if (num5 > max) max = num5;
+if (num6 > max) max = num6;
+if (num7 > max) max = num7;
+if (num8 > max) max = num8;
+if (num9 > max) max = num9;
+if (num10 > max) max = num10;
+
+Console.Write("Максимальное число равно -> ");
+Console.WriteLine(max);
