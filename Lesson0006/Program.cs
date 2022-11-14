91f2a9e73605c09658b6f7a22219f0521941e3eb
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-14 18:56:11 +0300

    какая-то ошибка

--- Lesson0006/Program.cs
+++ Lesson0006/Program.cs
@@ -13,43 +13,43 @@ Console.WriteLine(name);
 // Запрос пользователю ввести числа
 
 Console.Write("Введите 1 число -> ");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2 число -> ");
-int num1 = int.Parse(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 
 
 // запрос пользователю ввести оператор +, -, *, /, %
 
 Console.Write("Введите оператор +, -, *, /, %");
-string op = Console.ReadLine();
+string nam = Console.ReadLine();
 
 // Проверка условий
 
-if (op == '+'){
-    Console.Write("Сумма чисел равна -> ");
-    Console.WriteLine(num + num1);
-}
+// if (nam == '+'){
+//     Console.Write("Сумма чисел равна -> ");
+//     Console.WriteLine(num + num1);
+// }
 
-else if(op == '-'){
-    Console.Write("Вычитание чисел равна -> ");
-    Console.WriteLine(num - num1);
-}
+// else if(op == '-'){
+//     Console.Write("Вычитание чисел равна -> ");
+//     Console.WriteLine(num - num1);
+// }
 
-else if(op == '*'){
-    Console.Write("Произведение чисел равна -> ");
-    Console.WriteLine(num * num1);
-}
+// else if(op == '*'){
+//     Console.Write("Произведение чисел равна -> ");
+//     Console.WriteLine(num * num1);
+// }
 
-else if(op == '/'){
-    Console.Write("Деление чисел равна -> ");
-    Console.WriteLine(num / num1);
-}
+// else if(op == '/'){
+//     Console.Write("Деление чисел равна -> ");
+//     Console.WriteLine(num / num1);
+// }
 
-else if(op == '%'){
-    Console.Write("Деление по модулю чисел равна -> ");
-    Console.WriteLine(num % num1);
-}
+// else if(op == '%'){
+//     Console.Write("Деление по модулю чисел равна -> ");
+//     Console.WriteLine(num % num1);
+// }
 
-else{
-    Console.WriteLine("Вы ввели не тот оператор");
-}
+// else{
+//     Console.WriteLine("Вы ввели не тот оператор");
+// }
