a3504c5b6dfea32ed9a0a25414885e8df0eee69e
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-14 20:51:19 +0300

    усовершенствовали код по нахождению максимального числа из 9 чисел при помощи массива и функций

---
+++ Lesson0010/Program.cs
@@ -0,0 +1,17 @@
+﻿// усовершенствовать код по нахождению максимального числа среди 9 чисел при помощи массива и функций
+
+int Max(int a, int b, int c){
+    int result = a;
+    if (result < b) result = b;
+    if (result < c) result = c;
+
+    return result;
+}
+
+int[] array = {22, 55, 55,78, 22, 1, 599, 8, 3135, 68};
+
+int max = Max(Max(array[0], array[1], array[2]),
+            Max(array[3], array[4], array[5]),
+            Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
